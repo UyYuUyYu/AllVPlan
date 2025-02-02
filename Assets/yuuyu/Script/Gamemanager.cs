7a327776eb63145118de08f771243e0259e7beef
UyYuUyYu <104560044+UyYuUyYu@users.noreply.github.com>
2025-02-02 13:27:29 +0900

    AddGameManager

---
+++ Assets/yuuyu/Script/Gamemanager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemanager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //移動と相手の奥からくる弾を避ける
+    public void Phase1()
+    {
+        
+    }
+    public void Phase2()
+    {
+        
+    }
+    public void Phase3()
+    {
+        
+    }
+}
--- Assets/yuuyu/Script/UIScript/GameUIManager.cs
+++ Assets/yuuyu/Script/UIScript/GameUIManager.cs
@@ -32,4 +32,7 @@ public class GameUIManager : MonoBehaviour
 
        
     }
+
+    
+
 }
