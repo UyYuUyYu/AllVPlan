9f9f90a47ace803c6e681e92c7eb86e1f0b97d1a
UyYuUyYu <104560044+UyYuUyYu@users.noreply.github.com>
2025-04-25 00:16:31 +0900

    Addsubsc

---
+++ Assets/yuuyu/Script/StageScripts/StageManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StageManager : MonoBehaviour
+{
+    int _koeruSubscriber;
+    float subscribeMoney;
+    int targetSubscribeMoney;
+    [SerializeField] int ratioInreeesSubscription;
+    [SerializeField] Text targetSubscribeMoneyText,subscribeMoneyText;
+    
+    void Awake()
+    {
+        subscribeMoney=0;
+        string sceneName=SceneManager.GetActiveScene().name;
+        if(sceneName=="Stage1")
+        {
+            targetSubscribeMoney=1000;
+        }
+        else if(sceneName=="Stage2")
+        {
+            targetSubscribeMoney=2000;
+        }
+        else if(sceneName=="TestYuuyu2")
+        {
+            targetSubscribeMoney=200;
+        }
+        _koeruSubscriber=GameManager.koeruSubscriber;
+        print(_koeruSubscriber);
+        targetSubscribeMoneyText.text=targetSubscribeMoney.ToString("f0");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DefaultIncreesSubscribe();
+        if((int)subscribeMoney==targetSubscribeMoney)
+        {
+            print("Clear");
+        }
+
+    }
+
+    void DefaultIncreesSubscribe()
+    {
+        subscribeMoneyText.text=subscribeMoney.ToString("f0");
+        subscribeMoney+=ratioInreeesSubscription*_koeruSubscriber*Time.deltaTime;
+    }
+}
