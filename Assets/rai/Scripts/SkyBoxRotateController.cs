cfa38bc1d994773e60a2ea15a83d6cb9511a3370
mm_na <59761462+rum0504@users.noreply.github.com>
2025-05-01 18:15:48 +0900

    ステージ配置

--- Assets/rai/Scripts/SkyBoxRotateController.cs
+++ Assets/rai/Scripts/SkyBoxRotateController.cs
@@ -4,12 +4,10 @@ using UnityEngine;
 
 public class SkyBoxRotateController : MonoBehaviour
 {
-    public Transform cameraTransform;
+    public float rotationSpeed = 1.0f;
 
     void Update()
     {
-        // カメラの回転に合わせてSkyboxの正面が常に+Zを向くように調整
-        float yaw = cameraTransform.eulerAngles.y;
-        RenderSettings.skybox.SetFloat("_Rotation", yaw);
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * -rotationSpeed);
     }
 }
